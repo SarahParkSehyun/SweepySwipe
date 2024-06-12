d8758e2a5c11e1dee21b2ef7aff6c8eca905a2e4
hajin <kimhajin01@naver.com>
2024-06-12 22:43:58 +0900

    로그인 경로 수정

--- src/pages/SocialKakao/index.tsx
+++ src/pages/SocialKakao/index.tsx
@@ -1,6 +1,6 @@
 const SocialKakao = () => {
-  const Rest_api_key = 'bbf944c9e84c07e8f2cf4fe272cbf5de'; // REST API KEY
-  const redirect_uri = 'http://localhost:3000/auth'; // Redirect URI
+  const Rest_api_key = "4233737e445aa79b6a07cc26eeb6016c"; // REST API KEY
+  const redirect_uri = "http://localhost:8080/callback"; // Redirect URI
   // oauth 요청 URL
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
 
@@ -10,9 +10,7 @@ const SocialKakao = () => {
 
   return (
     <>
-      <button onClick={handleLogin}>
-        카카오 로그인
-      </button>
+      <button onClick={handleLogin}>카카오 로그인</button>
     </>
   );
 };
